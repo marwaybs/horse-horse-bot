828b0dc30fcaf36035cca3955f82cf806e2968b6
marwaybs <bsmarway@gmail.com>
2022-08-04 18:19:20 -0400

    switched to .env + starter code

---
+++ index.js
@@ -0,0 +1,30 @@
+// Require the necessary discord.js classes
+import { Client, GatewayIntentBits } from 'discord.js';
+import { token, guildId, clientId } from './config.json' assert {type: 'json'};
+import { SlashCommandBuilder, Routes } from 'discord.js';
+import { REST } from '@discordjs/rest';
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token.token);
+
+
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
