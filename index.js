aa1f097424dde3d1e3a9183dd40de8140e27f933
marwaybs <bsmarway@gmail.com>
2022-08-09 15:14:00 -0400

    added samples interactions

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ client.login(token);
 const commands = [
 	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
 	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('new').setDescription('please appear'),
 	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
 ]
 	.map(command => command.toJSON());
@@ -30,3 +29,17 @@ const rest = new REST({ version: '10' }).setToken(token);
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
+
+	client.on('interactionCreate', async interaction => {
+		if (!interaction.isChatInputCommand()) return;
+	
+		const { commandName } = interaction;
+	
+		if (commandName === 'ping') {
+			await interaction.reply('Pong!');
+		} else if (commandName === 'server') {
+			await interaction.reply('Server info.');
+		} else if (commandName === 'user') {
+			await interaction.reply('User info.');
+		}
+	});
